1cb189a5e2b37923498ecfcbff2bc36c570dc4f3
DeadlyKitten1234 <sava.a.delev@gmail.com>
2024-08-06 12:58:58 +0300

    Minor changes

--- include/EvalValues.h
+++ include/EvalValues.h
@@ -5,7 +5,7 @@
 using std::max;
 
 //Values(except king) taken from Stockfish and rescaled so pawn is 100 points
-const int16_t pieceValue[6] = { /*King value: INF = 10000*/10000, 1220, 400, 375, 610, 100 };
+const int16_t pieceValue[6] = { /*King value: INF*/30000, 1220, 400, 375, 610, 100 };
 
 //Values taken from https://www.chessprogramming.org/Simplified_Evaluation_Function
 //IMPORTANT: These values are formated like we see the board; But to the computer, 
@@ -125,9 +125,8 @@ inline int8_t getKingSqBonus(int8_t sq, const int16_t endgameWeight = 0) {
 	if constexpr (!black) {
 		sq = 63 - sq;
 	}
-	//Don't waste time, because * is slower than ==
 	if (endgameWeight == 0) {
 		return sqBonusMidgame[KING][sq];
 	}
-	return (sqBonusMidgame[KING][sq] * (128 - endgameWeight) + sqKingBonusEndgame[sq] * endgameWeight) >> 7;
+	return (sqBonusMidgame[KING][sq] * (128 - endgameWeight) + sqKingBonusEndgame[sq] * endgameWeight) / 128;
 }
--- main.cpp
+++ main.cpp
@@ -28,13 +28,7 @@ TODO:
     -change
     .make more evaluations to ordering
 
-    .<IMPORTAINT>
-    tweak formula for history heuristic in AI.h; l 131, 245
-    pretty sure found good one, but need to get fast sqrt algorithm
-
-
     -add
-    .add bonuses for sq
     .add draws by insufficient material and repetition of moves
     .add some tests? (take them from chess.com puzzles) in TestMaganer
 
@@ -74,7 +68,6 @@ int main(int argc, char* argv[]) {
     //char fen[] = "8/3KP3/8/8/8/8/8/6kq b - - 0 1";
     //char fen[] = "8/3r4/3k4/8/8/3K4/8/8 b - - 0 1";
     //char fen[] = "8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - - 0 1";
-    //char fen[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     //char fen[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
     char fen[] = "r1bqk2r/ppp2ppp/2np1n2/2b1p3/2B1P3/2NP1N2/PPP2PPP/R1BQK2R b KQkq - 0 1";
     //reverseFenPosition(fen);
